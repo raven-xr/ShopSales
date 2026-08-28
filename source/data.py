ccca041a3ffe6034b5ce7a210a0aa6c0e5326182
Raven <raven-esq@yandex.ru>
2026-08-28 14:52:13 +0700

    Improve the method of price generation
    Now, you'll get beautiful numbers:
    0.99
    699.99
    321.97
    17.01
    and etc

--- source/data.py
+++ source/data.py
@@ -1,7 +1,24 @@
+from math import ceil
+import random
 from psycopg2 import connect
 from faker import Faker
 import faker_commerce
-import random
+
+
+SPECIAL_PRICES = [
+    '.99',
+    '.97',
+    '.95',
+    '.69',
+    '.67',
+    '.49',
+    '.01'
+]
+
+
+def random_element(array: list):
+    index = random.randint(0, len(array) - 1)
+    return array[index]
 
 
 def generate_fake_products(count: int, min_cost: float = 0.99, max_cost = 999.99) -> None:
@@ -28,26 +45,9 @@ def generate_fake_products(count: int, min_cost: float = 0.99, max_cost = 999.99
         for _ in range(count):
             # Fake data
             name = fake.ecommerce_name()
-            price = str(fake.pyfloat(left_digits=3, right_digits=2, positive=True, min_value=0.99, max_value=999.99))
-            cost_price = str(round(float(price) * random.uniform(0.5, 0.9), 2))
+            price = str(random.randint(0, 999)) + random_element(SPECIAL_PRICES)
+            cost_price = str(ceil(float(price) * random.uniform(0.5, 0.9))) + random_element(SPECIAL_PRICES)
             discontinued = str(bool(random.randint(0, 1))).lower()
-            # Formatting the prices
-            if price.endswith(".0"):
-                price += "0"
-            else:
-                positive_digitals = "123456789"
-                for digital in positive_digitals:
-                    if price.endswith("." + digital):
-                        price += "0"
-                        break
-            if cost_price.endswith(".0"):
-                cost_price += "0"
-            else:
-                positive_digitals = "123456789"
-                for digital in positive_digitals:
-                    if cost_price.endswith("." + digital):
-                        cost_price += "0"
-                        break
             # Run SQL code
             cursor.execute(f"""
                 INSERT INTO products (product_name, price, cost_price, discontinued) VALUES
